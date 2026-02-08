68c3bb8003c5b6503141b2a802dfc730872cd7ed
frederik.jatzkowski <frederik.jatzkowski@check24.de>
2026-02-08 22:22:26 +0100

    allowed function pointers in global memory, allowing eg dynamic dispatch tables etc

--- pkg/hvil/program/global/initializer_call_ptr.go
+++ pkg/hvil/program/global/initializer_call_ptr.go
@@ -4,27 +4,43 @@ import (
 	"context"
 
 	"github.com/frederik-jatzkowski/havel/pkg/architecture/virtualmachine/assembly"
+	"github.com/frederik-jatzkowski/havel/pkg/hvil/pass/names"
+	"github.com/frederik-jatzkowski/havel/pkg/hvil/program/function"
 	"github.com/frederik-jatzkowski/havel/pkg/hvil/types"
 	"github.com/frederik-jatzkowski/havel/pkg/tool"
+	"github.com/frederik-jatzkowski/havel/pkg/tool/contexttool"
 )
 
 type InitializerCallPtr struct {
 	tool.Node[InitializerCallPtr]
+	names.NameResolution[struct {
+		Function *function.Function
+	}]
 
 	Name string `parser:"'call' '.' 'ptr' '(' @Ident ')'"`
 }
 
 func (node *InitializerCallPtr) ResolveNames(ctx context.Context) error {
-	// TODO: implement me
-	panic("implement me")
+	fn, err := contexttool.FromCtx[*function.Function](ctx, node.Name)
+	if err != nil {
+		return node.Wrap(err)
+	}
+
+	node.NameResolutionPass.Function = fn
+
+	return nil
 }
 
 func (node *InitializerCallPtr) ResolveTypes(expected types.Type) error {
-	//TODO implement me
-	panic("implement me")
+	if err := expected.CanBeAssignedDetailed(node.NameResolutionPass.Function.Signature()); err != nil {
+		return node.Wrap(err)
+	}
+
+	return nil
 }
 
 func (node *InitializerCallPtr) GenerateVirtualMachineAssembly(p *assembly.P) error {
-	// TODO: implement me
-	panic("implement me")
+	p.AddSLabel(node.NameResolutionPass.Function.NameResolutionPass.Entry.FullyQualifiedIdentifier())
+
+	return nil
 }
