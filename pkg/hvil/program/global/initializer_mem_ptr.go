a18b6e92786d563fcf5844552116e423b893b9ff
frederik.jatzkowski <frederik.jatzkowski@check24.de>
2026-02-08 21:18:17 +0100

    implemented simple global variables thar reference other global variables

--- pkg/hvil/program/global/initializer_mem_ptr.go
+++ pkg/hvil/program/global/initializer_mem_ptr.go
@@ -3,28 +3,56 @@ package global
 import (
 	"context"
 
+	"github.com/frederik-jatzkowski/havel/pkg/architecture/virtualmachine"
 	"github.com/frederik-jatzkowski/havel/pkg/architecture/virtualmachine/assembly"
+	"github.com/frederik-jatzkowski/havel/pkg/hvil/pass/names"
+	"github.com/frederik-jatzkowski/havel/pkg/hvil/pass/typecheck"
+	"github.com/frederik-jatzkowski/havel/pkg/hvil/program/function/block/instruction"
 	"github.com/frederik-jatzkowski/havel/pkg/hvil/types"
 	"github.com/frederik-jatzkowski/havel/pkg/tool"
+	"github.com/frederik-jatzkowski/havel/pkg/tool/contexttool"
 )
 
 type InitializerMemPtr struct {
 	tool.Node[InitializerMemPtr]
+	names.NameResolution[struct {
+		Decl *Decl
+	}]
+	typecheck.TypeCheck[struct {
+		Type types.Type
+	}]
 
 	Name string `parser:"'mem' '.' 'ptr' '(' @Ident ')'"`
 }
 
 func (node *InitializerMemPtr) ResolveNames(ctx context.Context) error {
-	// TODO: implement me
-	panic("implement me")
+	decl, err := contexttool.FromCtx[instruction.VarDecl](ctx, node.Name)
+	if err != nil {
+		return node.Wrap(err)
+	}
+
+	global, ok := decl.(*Decl)
+	if !ok {
+		return node.Errorf("declaration should be a global variable but was %T", decl)
+	}
+
+	node.NameResolutionPass.Decl = global
+
+	return nil
 }
 
 func (node *InitializerMemPtr) ResolveTypes(expected types.Type) error {
-	//TODO implement me
-	panic("implement me")
+	if err := (&types.Ref{}).EqualsDetailed(expected); err != nil {
+		return node.Wrap(err)
+	}
+
+	node.TypeCheckPass.Type = expected
+
+	return nil
 }
 
 func (node *InitializerMemPtr) GenerateVirtualMachineAssembly(p *assembly.P) error {
-	// TODO: implement me
-	panic("implement me")
+	p.AddSLit(node.TypeCheckPass.Type.Bytes(), virtualmachine.NewFatPtr(0, uint32(node.NameResolutionPass.Decl.RelAddr())).ToUint64())
+
+	return nil
 }
